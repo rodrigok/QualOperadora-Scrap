302cffa696d38edd2133d8bd786a1f8a4b846aa7
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2014-05-13 12:08:44 -0300

    Working

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/:phone', function(req, res) {
-	// var noodle = require('noodlejs');
-
-	// noodle.query({
-	// 	url:      'http://www.qualoperadora.net/',
-	// 	post: {
-	// 		ref2115: "http%3A%2F%2Fwww.qualoperadora.net%2F",
-	// 		telefone: '5196670281'
-	// 	},
-	// 	cache: false,
-	// 	// selector: '#resultado',
-	// 	//*[@id="resultado"]/span[1]
-	// 	//#resultado > span:nth-child(1)
-	// 	// extract:  'html'
-	// })
-	// .then(function (results) {
-	// 	res.send(arguments);
-	// });
-	// 
-	
 	var request = require('request');
-	request.post('http://www.qualoperadora.net/', {form:{ref2115: "http://www.qualoperadora.net/", telefone: "5196670281"}}, function(error, response, body) {
-		console.log(body)
+
+	var options = {
+		url: 'http://www.qualoperadora.net/',
+		method: 'POST',
+		headers: {
+			'cookie': 'PHPSESSID=pgflhdqa2ltmirougo2cjf79f0; _jsuid=3103488192; USID=3362150133-1399133560.37; _first_pageview=1; heatmaps_g2g_100536567=no; p_cachedDomain=www.qualoperadora.net; p_cachedDeals=%5B%5D; __utma=7597106.1768311926.1399133561.1399147733.1399992202.3; __utmb=7597106.2.10.1399992202; __utmc=7597106; __utmz=7597106.1399133561.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _eventqueue=%7B%22heatmap%22%3A%5B%5D%2C%22events%22%3A%5B%5D%7D; __utma=112029058.1497549882.1399133567.1399147736.1399992206.3; __utmb=112029058.2.10.1399992206; __utmc=112029058; __utmz=112029058.1399133567.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
+			'user-agent': req.get('user-agent'),
+			'accept-language': req.get('accept-language')
+		},
+		form:{
+			ref2115: "http://www.qualoperadora.net/",
+			telefone: req.params.phone
+		}
+	};
+
+	request(options, function(error, response, body) {
 		var cheerio = require('cheerio'),
 		$ = cheerio.load(body);
-
-		res.send($('#resultado').html());
 		
+		var resultado = $('#resultado > span');
+		
+		var data = {
+			telefone: resultado.eq(0).html(),
+			operadora: resultado.eq(1).html(),
+			portabilidade: resultado.eq(2).text().trim().toLowerCase() == 'sim',
+			estado: resultado.eq(3).html()
+		}
+		
+		res.send(data);
 	})
 });
 
